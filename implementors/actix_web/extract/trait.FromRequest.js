70e91e26b09ebb2ac71205e3377da7bc700b7fd0
simorex80 <82770876+simorex80@users.noreply.github.com>
2023-08-30 08:43:16 +0000

    Deploying to gh-pages from @ simorex80/actix-web@76f6106f8fe8b8d4827698d8132624080ae189aa 🚀

---
+++ implementors/actix_web/extract/trait.FromRequest.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {
+"actix_multipart":[["impl&lt;T&gt; FromRequest for <a class=\"struct\" href=\"actix_multipart/form/struct.MultipartForm.html\" title=\"struct actix_multipart::form::MultipartForm\">MultipartForm</a>&lt;T&gt;<span class=\"where fmt-newline\">where\n    T: <a class=\"trait\" href=\"actix_multipart/form/trait.MultipartCollect.html\" title=\"trait actix_multipart::form::MultipartCollect\">MultipartCollect</a>,</span>"],["impl FromRequest for <a class=\"struct\" href=\"actix_multipart/struct.Multipart.html\" title=\"struct actix_multipart::Multipart\">Multipart</a>"]],
+"actix_web":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
